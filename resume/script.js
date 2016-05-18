475b649c9e750db583472442284c05f13753a7c7
GEARWasHere <soahcdesrever@gmail.com>
2016-05-18 00:01:37 -0500

    Resume
    Init for resume files

---
+++ resume/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+  $(".education").append(smartly, udacityFrontEnd, ai, tcc, ptk);
+  $(".work").append(geeksquad, bbops, bbcomp, bbap, hideaway, changs, cheddars);
+  $(".references").append(amy, jt, rob, joe, andy);
+  $(".summary").delay(100).fadeIn(700);
+  $(".skillset").delay(300).fadeIn(700);
+  $(".education").delay(500).fadeIn(700);
+  $(".work").delay(700).fadeIn(700);
+  $(".references").delay(900).fadeIn(700);
+});
+
+//education
+
+var smartly = "<h2>Smartly<\/h2><br><p><i>Masters of Business Administration<\/i><br>Data Science<br>2016 - Current<br><\/p>";
+
+var udacityFrontEnd = "<h2>Udacity<\/h2><br><p><i>Nanodegree<\/i><br>Front-End Development<br>2015 - Current<br><\/p>";
+
+var ai = "<h2>The Art Institute of Austin<\/h2><br><p><i>Bachelors of Fine Arts<\/i><br>Graphic Design<br>2011 - Current<br><\/p>";
+
+var tcc = "<h2>Tulsa Community College<\/h2><br><p><i>Bachelors of Fine Arts<\/i><br>Graphic Design<br>2010 - 2011<br><\/p>";
+
+var ptk = "<h2>Phi Theta Kappa<\/h2><br><p><i>Honor Society<\/i><br>Academic Achievements<br>2010 - Current<br><\/p>"
+
+//work
+
+var geeksquad = "<h2>Geek Squad<\/h2><br><p><i>Consultation\/Ops Agent<\/i><br>2016 - Current<br>Responsible for inbound and outbound shipments of client units, initiating preservation of client data, and consulting with clients to reach solutions on restoring the client's unit and data.<\/p>";
+
+var bbops = "<h2>Best Buy<\/h2><br><p><i>Ops Inventory Specialist\/Merchandising Associate<\/i><br>2014 - Current<br>Responsible for inbound and outbound shipments of business and customer products, processing and fulfilling B2C and B2B orders, sorting inbound shipments of products, modifying weekly pricing, and setting up functional displays of new features and products.<\/p>";
+
+var bbcomp = "<h2>Best Buy<\/h2><br><p><i>Computing Sales Associate<\/i><br>2014 - 2015<br>Responsible for finding personal computing solutions for clients and businesses.<\/p>";
+
+var bbap = "<h2>Best Buy<\/h2><br><p><i>Asset Protection Specialist<\/i><br>2014 - 2015<br>Responsible for communicating and gaining intelligence to prevent company loss and theft and maintain business security.<\/p>";
+
+var hideaway = "<h2>Hideaway<\/h2><br><p><i>Line Cook\/Server<\/i><br>2013 - 2014<br>Responsible for running one half of a full kitchen, creating Italian-inspired American cuisine ranging from appetizers to entrees to desserts. On occasion, serve guests and perform various cleaning duties.<\/p>";
+
+var changs = "<h2>P.F. Chang\'s China Bistro<\/h2><br><p><i>Server<\/i><br>2013<br>Responsible for serving guests and performing various cleaning duties. <\/p>";
+
+var cheddars = "<h2>Cheddar\'s Casual Cafe<\/h2><br><p><i>Server<\/i><br>2012 - 2013<br>Responsible for serving guests and performing various cleaning duties. <\/p>";
+
+//references
+
+var amy = "<h2>Amy Simon<\/h2><br><p><i>Operations Supervisor at Best Buy<\/i><br>817-300-4283<\/p>";
+
+var rob = "<h2>Robert Wilder<\/h2><br><p><i>General Manager at Hideaway<\/i><br>918-497-6714<\/p>";
+
+var corbin = "<h2>Corbin Jones<\/h2><br><p><i>Solutions Consultant at Apple, Inc.<\/i><br>xxx-xxx-xxxx<\/p>";
+
+var andy = "<h2>Andy Brison<\/h2><br><p><i>Previous Manager at Cheddar\'s<\/i><br>512-348-5894<\/p>";
+
+var joe = "<h2>Joe Hutchinson<\/h2><br><p><i>Manager at Hideaway<\/i><br>918-406-8378<\/p>";
+
+var jt = "<h2>Jonathan Dollery<\/h2><br><p><i>Previous Computing Supervisor at Best Buy<\/i><br>903-744-5810<\/p>";
