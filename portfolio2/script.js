8028fe35b3e3a439a91d1622ada695da4c7fb831
GEARWasHere <soahcdesrever@gmail.com>
2016-06-21 12:23:08 -0500

    +nav

--- portfolio2/script.js
+++ portfolio2/script.js
@@ -1,4 +1,11 @@
-$(document).ready(function() {
-  $(".main").fadeIn(500);
-  $(".social").delay(500).fadeIn(500);
-})
+$(document).ready(function(){
+  $(".navicon").delay(500).fadeIn(500);
+  $(".main").delay(1000).fadeIn(500);
+  $(".social").delay(1500).fadeIn(500);
+  $(".navicon").click(function(){
+    $(".navigation").fadeIn(100);
+  });
+  $(".close").click(function(){
+    $(".navigation").fadeOut(100);
+  });
+});
