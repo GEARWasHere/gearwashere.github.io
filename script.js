9f7723aac2368ad4caf235ce852615f48af88fd2
GEARWasHere <soahcdesrever@gmail.com>
2016-06-24 01:55:59 -0500

    1.0
    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".navicon").delay(500).fadeIn(500);
+  $(".main").delay(1000).fadeIn(500);
+  $(".main2").delay(1000).fadeIn(500);
+  $(".social").delay(1500).fadeIn(500);
+  $(".opt").delay(2000).fadeIn(500);
+  $(".gear").delay(2500).fadeIn(1250);
+  $(".navicon").click(function(){
+    $(".navigation").fadeIn(100);
+  });
+  $(".close").click(function(){
+    $(".navigation").fadeOut(100);
+  });
+});
